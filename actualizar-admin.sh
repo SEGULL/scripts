45f7d58d97d8c69af9b5098cf20437c09455fe2c
SEGULL <alejandro.canahuire.hilari@github.com>
2017-06-01 14:30:02 -0500

    Update actualizar-admin.sh

--- actualizar-admin.sh
+++ actualizar-admin.sh
@@ -3,7 +3,7 @@
 #sudo apt-get -s upgrade || reboot
 DIA=`date +"%d/%m/%Y"`
 HORA=`date +"%H:%M"`
-if [ HORA >= `17:30`]
+if [ HORA > `17:30`]
   then
     echo "la fecha de termino  el mantenimiento es $DIA y la hora actual es $HORA!">>/home/scripts/reporte.txt && poweroff
 else
