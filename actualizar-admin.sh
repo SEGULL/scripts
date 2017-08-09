0dcddf89acb23be1db4cbf8d5204c7ff294294e7
SEGULL <alejandro.canahuire.hilari@github.com>
2017-08-09 15:57:44 -0500

    Update actualizar-admin.sh

--- actualizar-admin.sh
+++ actualizar-admin.sh
@@ -6,4 +6,4 @@ HORA=`date +"%H:%M"`
 sudo apt-get update || reboot
 sudo apt-get -s upgrade || reboot
     echo "la fecha de mantenimiento es el $DIA y la hora actual es $HORA!" >>/home/scripts/reporte.txt || reboot
-    echo "la fecha de termino  el mantenimiento es $DIA y la hora actual es $HORA!">>/home/scripts/reporte.txt && poweroff
+    echo "la fecha de termino  el mantenimiento es $DIA y la hora actual es $HORA!">>/home/scripts/reporte.txt 
